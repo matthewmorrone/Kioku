95960b459837305eadcd19c5bcefded097704624
Matthew Morrone <matthewmorrone1@gmail.com>
2026-02-24 10:39:39 -0500

    Initial Commit

---
+++ Kioku/KiokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  KiokuApp.swift
+//  Kioku
+//
+//  Created by Matthew Morrone on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct KiokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KiokuTests/KiokuTests.swift
@@ -0,0 +1,17 @@
+//
+//  KiokuTests.swift
+//  KiokuTests
+//
+//  Created by Matthew Morrone on 2/24/26.
+//
+
+import Testing
+@testable import Kioku
+
+struct KiokuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
